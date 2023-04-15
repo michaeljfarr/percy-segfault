b6d00be226670c606211219e94569dc18263ca17
Michael Farr <michaeljfarr@hotmail.com>
2023-04-15 19:29:09 +1000

    add a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Builder } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome";
+const percySnapshot = require("@percy/selenium-webdriver");
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("test 1", async () => {
+  console.log("at start of test 1");
+  const driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(new chrome.Options().addArguments("--incognito"))
+    .build();
+  await driver.get(`https://create-react-app.dev/`);
+  await percySnapshot(driver, "a page", { widths: [1280] });
+  console.log("at end of test 1");
+});
+
+test("test 2", async () => {
+  console.log("at start of test 2");
+  console.log("at end of test 2");
 });
